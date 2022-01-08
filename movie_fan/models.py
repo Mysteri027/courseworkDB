b0c749bbd20463adf3a8020a975a992580dc6f38
IGOR <igor.sheplyakov@yandex.ru>
2022-01-08 22:57:14 +0300

    Initial commit

---
+++ movie_fan/models.py
@@ -0,0 +1,39 @@
+import datetime
+
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    login = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    date_registration = models.DateField(default=datetime.date.today())
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=255)
+    birth_date = models.DateField(default='1970-01-01')
+    photo = models.ImageField()
+    bio = models.CharField(max_length=255)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    mark = models.IntegerField()
+    rating = models.FloatField()
+    poster = models.ImageField()
+    trailer = models.CharField(max_length=255)
+    production_year = models.IntegerField()
+    country = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    producer = models.CharField(max_length=50)
+
+
+class Review(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField(default=datetime.date.today())
+    likes_count = models.IntegerField()
+    dislikes_count = models.IntegerField()
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
