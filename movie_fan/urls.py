50b99540f3e00ebf7f37257a20487eb955bfaea5
IGOR <igor.sheplyakov@yandex.ru>
2022-01-09 14:48:19 +0300

    2

--- movie_fan/urls.py
+++ movie_fan/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='main'),
+    path('news/', news, name='news'),
+    path('registration/', registration, name='registration'),
+    path('thriller/', thriller, name='thriller'),
+    path('detective/', detective, name='detective'),
+    path('family/', family, name='family'),
+    path('comedy', comedy, name='comedy'),
 ]
